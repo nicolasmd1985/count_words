e0ab83fb247c0c83a42ff82b309ba049198e78a1
Nicolas Mahecha <nicolas.mahecha@dipzo.net>
2022-03-30 15:33:36 -0400

    fix app

--- app/controllers/count_word_controller.rb
+++ app/controllers/count_word_controller.rb
@@ -12,10 +12,11 @@ class CountWordController < ApplicationController
       if sentence.class == Array
         # result = sentence.join('').gsub(/\d+/, "").split.map{|x| ["word: #{x} size: #{x.size}"]}
         sentence.map{|x| x.gsub(/\d+/, "").gsub(/[^a-zA-Z ]/,'')}.each do |se|
-          result << se.split.each_with_index.map{ |x,y|  ["{word: #{x} , count: #{y+1}}"]}
+          result << se.split.each_with_index.map{ |x,y|  ["{word: #{x} , count: #{y+1}, ocurrency: #{se.scan(x).count}}"]}
         end
       else
-        result = sentence.gsub(/\d+/, "").gsub(/[^a-zA-Z ]/,'').split.each_with_index.map{|x,y| ["{word: #{x} , count: #{y+1}}"]}
+        clean_sentence = sentence.gsub(/\d+/, "").gsub(/[^a-zA-Z ]/,'')
+        result = clean_sentence.split.each_with_index.map{|x,y| ["{word: #{x} , count: #{y+1}, ocurrency: #{clean_sentence.scan(x).count}}"]}
         # result= sentence.tr("0-9", "").split.map{|x| [x ,x.size]}
       end
     end
